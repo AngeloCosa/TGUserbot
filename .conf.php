d4decc4319af48f9483ab1a1762ed1b932606ca5
AngeloCosa <60105488+AngeloCosa@users.noreply.github.com>
2020-01-20 17:19:07 +0100

    Update .conf.php

--- .conf.php
+++ .conf.php
@@ -1,6 +1,6 @@
 <?php $conf = array (
-  'dir' => '5e243a1ca8ad3',
-  'password' => '$2y$10$wqXTsx6f1O1u3OcZP1UP3u2J5OluFOtWBMOLX0p/qMbJLNLMIIysK',
-  'session' => '5e243a1ca8d91',
+  'dir' => '',
+  'password' => '',
+  'session' => '',
   'v' => '1.2',
 );
