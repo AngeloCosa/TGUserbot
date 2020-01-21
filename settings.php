690b4bce3168d64fded0b9c989c501a0b3803fc3
AngeloCosa <60105488+AngeloCosa@users.noreply.github.com>
2020-01-21 18:41:21 +0100

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,33 @@
+<?php
+
+$settings = array (
+  'language' => 'it',
+  'bot_file' => 'bot.php',
+  'readmsg' => false,
+  'send_errors' => false,
+  'always_online' => false,
+  'auto_reboot' => true,
+  'madelineCli' => false,
+  'send_data' => true,
+  'madelinePhar' => 'madeline.php',
+);
+
+$madelineSettings = array (
+  'app_info' => 
+  array (
+    'api_id' => 6,
+    'api_hash' => 'eb06d4abfb49dc3eeb1aeb98ae0f581e',
+    'lang_code' => 'en',
+    'app_version' => '5.9.0',
+    'device_model' => 'Asus ASUS_Z00ED',
+    'system_version' => 'Android Nougat MR1 (25)',
+  ),
+  'logger' => 
+  array (
+    'logger' => 0,
+  ),
+  'secret_chats' => 
+  array (
+    'accept_chats' => false,
+  ),
+);
