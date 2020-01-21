bd6bc49489320940e0a7ffad5ef16d48d37efd6b
AngeloCosa <60105488+AngeloCosa@users.noreply.github.com>
2020-01-21 18:43:24 +0100

    Add files via upload

---
+++ .conf.php
@@ -0,0 +1,6 @@
+<?php $conf = array (
+  'dir' => '5e243a1ca8ad3',
+  'password' => '$2y$10$wqXTsx6f1O1u3OcZP1UP3u2J5OluFOtWBMOLX0p/qMbJLNLMIIysK',
+  'session' => '5e243a1ca8d91',
+  'v' => '1.2',
+);
